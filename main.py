2535f84040066695c582a51068135cca14978d68
lucena <john@caixabaixa.com>
2014-04-08 15:26:15 -0700

    First cut at an actual working site.

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ Example Genomics Map Reduce
 import datetime
 import jinja2
 import json
+import logging
 import os
+import time
 import webapp2
 
 from google.appengine.api import users
-
+from google.appengine.api.logservice import logservice
+from collections import OrderedDict
 from common import Common
 
 Common.initialize()
@@ -33,34 +36,29 @@ JINJA_ENVIRONMENT = jinja2.Environment(
   autoescape=True,
   extensions=['jinja2.ext.autoescape'])
 
+LOG_LEVELS = {
+              logservice.LOG_LEVEL_DEBUG: "Debug",
+              logservice.LOG_LEVEL_INFO: "Info",
+              logservice.LOG_LEVEL_WARNING: "Warning",
+              logservice.LOG_LEVEL_ERROR: "Error",
+              logservice.LOG_LEVEL_CRITICAL: "Critical",
+              }
+
+PAGES = OrderedDict([
+         ('/', "Home"),
+         ('/products', "Products"),
+         ('/purchase', "Purchase"),
+         ('/login', "Login"),
+         ('/contact', "Contact"),
+         ])
 
 class MainHandler(webapp2.RequestHandler):
-  """The main page that users will interact with, which presents users with
-  the ability to upload new data or run MapReduce jobs on their existing data.
-  """
-
-  # Provide default settings for the user that they can then override.
-  DEFAULT_SETTINGS = {
-    'readsetId': "CJ_ppJ-WCxDxrtDr5fGIhBA=",
-    'sequenceName': "chr20",
-    'sequenceStart': 68101,
-    'sequenceEnd': 68164,
-    'useMockData': False,
-    'runPipeline': False,
-  }
+  """The main page."""
 
   def get(self):
     user = users.get_current_user()
     if user:
-      username = users.User().nickname()
-      template = JINJA_ENVIRONMENT.get_template('index.html')
-      self.response.out.write(template.render({
-        "username": username,
-        "version": self._get_version(),
-        "targets": {},
-        "settings": MainHandler.DEFAULT_SETTINGS,
-      }))
-
+      self._render_page(None, None)
     else:
       template = JINJA_ENVIRONMENT.get_template('grantaccess.html')
       self.response.write(template.render({
@@ -68,57 +66,17 @@ class MainHandler(webapp2.RequestHandler):
       }))
 
   def post(self):
-    # Collect inputs.
-    readsetId = self.request.get("readsetId")
-    sequenceName = self.request.get('sequenceName')
-    sequenceStart = int(self.request.get('sequenceStart'))
-    sequenceEnd = int(self.request.get('sequenceEnd'))
-    useMockData = self.request.get('useMockData')
-
-    # TODO: Validate inputs such as sequence start and end to make
-    # sure they are in bounds based on TARGETS.
-
-    reads = []
+    infoMessage = None
     errorMessage = None
 
-    if self.request.get("submitRead"):
-      # Use the API to get the requested data.
-      # If you are running the real pipeline map reduce then hit it.
-      if self.request.get('runPipeline'):
-        self.redirect("foo")
-        return
-
-    elif self.request.get("submitReadSample"):
-      # Read in local sample data which is based off of default settings.
-      readsetId = MainHandler.DEFAULT_SETTINGS['readsetId']
-      sequenceName = MainHandler.DEFAULT_SETTINGS['sequenceName']
-      sequenceStart = MainHandler.DEFAULT_SETTINGS['sequenceStart']
-      sequenceEnd = MainHandler.DEFAULT_SETTINGS['sequenceEnd']
-      path = os.path.join(os.path.split(__file__)[0],
-                          'static/reads-search_sample_data.json')
-      file = open(path, 'r')
-      content = file.read()
-      file.close()
-      content = json.loads(content)
-      reads = content['reads']
-
-    # Render template with results or error.
-    username = users.User().nickname()
-    template = JINJA_ENVIRONMENT.get_template('index.html')
-    self.response.out.write(template.render({
-      "username": username,
-      "version": self._get_version(),
-      "targets": {},
-      "settings": {
-        'readsetId': readsetId,
-        'sequenceName': sequenceName,
-        'sequenceStart': sequenceStart,
-        'sequenceEnd': sequenceEnd,
-        'useMockData': useMockData,
-      },
-      "errorMessage": errorMessage,
-      "results": coverage,
-    }))
+    if self.request.get("submitCreate"):
+      level = int(self.request.get("logLevel"))
+      message = str(self.request.get("logMessage"))
+      self._log_message(level, message)
+      infoMessage = str.format("Logged the following: %s - %s" %
+                               (LOG_LEVELS[level], message))
+    # Render the page.
+    self._render_page(infoMessage, errorMessage)
 
   def _get_version(self):
     version = self.request.environ["CURRENT_VERSION_ID"].split('.')
@@ -128,9 +86,62 @@ class MainHandler(webapp2.RequestHandler):
       date = datetime.datetime.now()
     return name + " as of " + date.strftime("%Y-%m-%d %X")
 
+  def _get_page_name(self):
+    return PAGES[self.request.path] if self.request.path in PAGES else "Unknown"
+
+  def _log_message(self, level, message):
+    if level == logservice.LOG_LEVEL_DEBUG:
+       logging.debug(message)
+    elif level == logservice.LOG_LEVEL_INFO:
+       logging.info(message)
+    elif level == logservice.LOG_LEVEL_WARNING:
+       logging.warning(message)
+    elif level == logservice.LOG_LEVEL_ERROR:
+       logging.error(message)
+    elif level == logservice.LOG_LEVEL_CRITICAL:
+       logging.critical(message)
+
+  def _get_log_messages(self):
+    logs = []
+    count = 20
+    index = 1
+    for log in logservice.fetch(end_time=time.time(), offset=None,
+                                minimum_log_level=logservice.LOG_LEVEL_DEBUG,
+                                include_app_logs=True):
+      log.timestamp = datetime.datetime.fromtimestamp(log.start_time)
+      for app_log in log.app_logs:
+        app_log.timestamp = datetime.datetime.fromtimestamp(app_log.time)
+        app_log.level_name = LOG_LEVELS[app_log.level]
+      logs.append(log)
+      index += 1
+      if index > count:
+        break
+    return logs
+
+  def _render_page(self, infoMessage, errorMessage):
+    username = users.User().nickname()
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    logs = self._get_log_messages()
+    self.response.out.write(template.render({
+      "pages": PAGES,
+      "path": self.request.path,
+      "pagename": self._get_page_name(),
+      "username": username,
+      "version": self._get_version(),
+      "infoMessage": infoMessage,
+      "errorMessage": errorMessage,
+      "logs": self._get_log_messages(),
+    }))
+
+
+# For demo purposes, map many sample pages to this handler.
 app = webapp2.WSGIApplication(
   [
     ('/', MainHandler),
+    ('/products', MainHandler),
+    ('/purchase', MainHandler),
+    ('/login', MainHandler),
+    ('/contact', MainHandler),
   ],
   debug=True)
 
