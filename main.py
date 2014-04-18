710d8da99124b63cd7b7a113baaf67556e4d8258
lucena <john@caixabaixa.com>
2014-04-18 15:25:13 -0700

    Support event creation in GA.

--- main.py
+++ main.py
@@ -84,12 +84,24 @@ class MainHandler(webapp2.RequestHandler):
     alertLevel = None
 
     # Log a message if requested.
-    if self.request.get("submitCreate"):
+    if self.request.get("submitCreateLog"):
       logLevel = int(self.request.get("logLevel"))
       logMessage = str(self.request.get("logMessage"))
       self._log_message(logLevel, logMessage)
       alertMessage = str.format("Logged the following: %s - %s" %
                                (LOG_LEVELS[logLevel], logMessage))
+      alertLevel = "alert-info"
+
+    # Log a message if requested.
+    if self.request.get("submitCreateEvent"):
+      eventCategory = str(self.request.get("eventCategory"))
+      eventAction = str(self.request.get("eventAction"))
+      eventLabel = str(self.request.get("eventLabel"))
+      eventValue = str(self.request.get("eventValue"))
+      self._post_event_to_ga(eventCategory, eventAction, eventLabel, eventValue)
+      alertMessage = str.format("Created the following event in Google "
+          "Analytics %s : %s : %s : %s" %
+          (eventCategory, eventAction, eventLabel, eventValue))
       alertLevel = "alert-info"
 
     # Crash the page if requested.
@@ -216,20 +228,36 @@ class MainHandler(webapp2.RequestHandler):
         description = str.format("%s - %s - %s" % (timestamp, level, message))
         isFatal = 1 if app_log.level == logservice.LOG_LEVEL_CRITICAL else 0
         count += 1
-        self._post_to_ga("exception", description, isFatal)
+        self._post_exception_to_ga(description, isFatal)
     # Save your place.
     self._update_analytics_status(lastpushtime, offset)
     return count
 
-  def _post_to_ga(self, type, description, isFatal):
+  def _post_event_to_ga(self, category, action, label, value):
+    additional_fields = {
+      "t": "event",
+      "ec": category,
+      "ea": action,
+      "el": label,
+      "ev": value,
+    }
+    self._post_to_ga(additional_fields)
+
+  def _post_exception_to_ga(self, description, isFatal):
+    additional_fields = {
+      "t": "exception",
+      "exd": description,
+      "exf": isFatal,
+    }
+    self._post_to_ga(additional_fields)
+
+  def _post_to_ga(self, additional_fields):
     form_fields = {
       "v": "1",
       "tid": os.environ['GA_PROPERTY_ID'],
       "cid": GA_ANONYMOUS_CLIENT_ID,
-      "t": type,
-      "exd": description,
-      "exf": isFatal,
     }
+    form_fields.update(additional_fields)
     form_data = urllib.urlencode(form_fields)
     result = urlfetch.fetch(url=GA_URL_ENDPOINT,
         payload=form_data,
